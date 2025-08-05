39b952987afe6b9d8bfae727b28b90de36233d52
praveena415 <chinthapraveena194@gmail.com>
2025-08-05 21:54:49 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,25 +1,16 @@
 import React from "react";
-import { ThemeProvider, useTheme } from "./ThemeContext";
-import NestedComponent from "./components/NestedComponent";
-import "./index.css";
-
-const ThemeToggler = () => {
-  const { theme, toggleTheme } = useTheme();
-
-  return (
-    <div className={`app-container ${theme}`}>
-      <h1>Current Theme: {theme}</h1>
-      <button onClick={toggleTheme}>Toggle Theme</button>
-      <NestedComponent />
-    </div>
-  );
-};
+import { AuthProvider } from "./AuthContext";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <ThemeToggler />
-    </ThemeProvider>
+    <AuthProvider>
+      <Navbar />
+      <Main />
+      <Footer />
+    </AuthProvider>
   );
 };
 
